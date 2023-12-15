b4168d1ea79b5255b88688c0921b90a87d2e5136
Ali Rezaei <alirezaeim1380@gmail.com>
2023-12-15 17:41:01 +0330

    initial commit

---
+++ src/test/java/com/unittest/codecoverage/samples/MyCustomList.java
@@ -0,0 +1,17 @@
+package test.java.com.unittest.codecoverage.samples;
+
+import java.util.AbstractList;
+
+public class MyCustomList extends AbstractList<String>{
+
+	@Override
+	public String get(int arg0) {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+}
