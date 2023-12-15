1d39fe7c1c74c9db896a7c5fdb245157275d4e21
armin <armin.delgosarmaher@huma.com>
2023-12-15 19:13:26 +0330

    Add tests for Code coverage base app class

--- src/test/java/com/unittest/codecoverage/CodecoverageApplicationTests.java
+++ src/test/java/com/unittest/codecoverage/CodecoverageApplicationTests.java
@@ -1,13 +1,25 @@
 package test.java.com.unittest.codecoverage;
 
+import com.unittest.codecoverage.CodecoverageApplication;
+import com.unittest.codecoverage.repositories.PersonRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-class CodecoverageApplicationTests {
+import java.util.ArrayList;
+import org.mockito.Mockito;
 
-//	@Test
-	void contextLoads() {
+class CodeCoverageApplicationTests {
+
+	@Mock
+	SpringApplication app;
+
+	@Test
+	public void testMain_whileLoadingMainFunction() {
+		String[] args = {"arg1", "arg2"};
+		CodecoverageApplication.main(args);
+		Mockito.verify(app).run();
 	}
 
 }
